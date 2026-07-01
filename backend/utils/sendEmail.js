1d1a3d3b51d7d79b2b459b7601f66289dfc05b78
dalalanshraj <dalalanshraj@gmail.com>
2026-07-02 01:22:23 +0530

    ok

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -12,7 +12,7 @@ const transporter = nodemailer.createTransport({
 });
 const sendEmail = async (to, subject, html) => {
   const info = await transporter.sendMail({
-    from: `"calypso401(Julie Shurden)" <${process.env.EMAIL_USER}>`,
+    from: `"Bill Roberson" <${process.env.EMAIL_USER}>`,
     to,
     subject,
     html,
